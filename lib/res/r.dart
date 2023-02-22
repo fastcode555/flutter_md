64267700c288946e6c0820f5899920cf95bb2daa
Barry <87644022+fastcode555@users.noreply.github.com>
2023-02-22 15:07:38 +0700

    feat:为该项目增加应用图标

--- lib/res/r.dart
+++ lib/res/r.dart
@@ -1,15 +1,15 @@
 class R {
-	///------------------------ assets ------------------------
-	static const String icFreely = 'assets/ic_freely.svg';
-	static const String icMdEye = 'assets/ic_md_eye.png';
-	static const String icMdSplit = 'assets/ic_md_split.png';
-	static const String icNewFile = 'assets/ic_new_file.png';
-	static const String icNormally = 'assets/ic_normally.svg';
-	static const String icSqueezed = 'assets/ic_squeezed.svg';
-	static const String icFramework = 'assets/ic_framework.svg';
-	static const String icChangeLine = 'assets/ic_change_line.png';
-	static const String icLargeFrame = 'assets/ic_large_frame.svg';
-	static const String icTextDelete = 'assets/ic_text_delete.png';
-	static const String icSplitSymbol = 'assets/ic_split_symbol.png';
-	static const String icNoIndentation = 'assets/ic_no_indentation.svg';
+  ///------------------------ assets ------------------------
+  static const String icFreely = 'assets/ic_freely.svg';
+  static const String icMdEye = 'assets/ic_md_eye.png';
+  static const String icMdSplit = 'assets/ic_md_split.png';
+  static const String icNewFile = 'assets/ic_new_file.png';
+  static const String icNormally = 'assets/ic_normally.svg';
+  static const String icSqueezed = 'assets/ic_squeezed.svg';
+  static const String icFramework = 'assets/ic_framework.svg';
+  static const String icChangeLine = 'assets/ic_change_line.png';
+  static const String icLargeFrame = 'assets/ic_large_frame.svg';
+  static const String icTextDelete = 'assets/ic_text_delete.png';
+  static const String icSplitSymbol = 'assets/ic_split_symbol.png';
+  static const String icNoIndentation = 'assets/ic_no_indentation.svg';
 }
